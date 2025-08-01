87c337006902ab5d19f1201336be6a6edc479cd1
Yosub Shin <yosub.shin.0@gmail.com>
2025-07-31 22:24:48 -1000

    No early stopping for now

--- run_training.sh
+++ run_training.sh
@@ -75,7 +75,7 @@ python train_repurpose.py \
     --log_level INFO \
     --use_wandb \
     --wandb_project "repurpose-experiments" \
-    --early_stopping_patience 20 \
+    --early_stopping_patience 0 \
     --gradient_clip 0.1 \
     --precision "16-mixed" \
     --enable_checkpointing \
