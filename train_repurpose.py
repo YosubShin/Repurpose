ebf8610fe6fef30ac67faca89d37403ef3f1669f
Yosub Shin <yosub.shin.0@gmail.com>
2025-08-11 22:10:28 -1000

    Revert "use mask always"
    This reverts commit 7e737993e64349e7024e834ee2d17356531a042d.

--- train_repurpose.py
+++ train_repurpose.py
@@ -1012,7 +1012,7 @@ class EndOfEpochVisualizationCallback(Callback):
 
                             # Get predictions - now includes regression output
                             logit_a, logit_v, logit_f, offset_f = pl_module(
-                                audio, visual, caption, mask=seq_mask)
+                                audio, visual, caption)
 
                             log_memory_usage(
                                 self.logger, f"{dataset_name} batch {batch_idx} after inference")
